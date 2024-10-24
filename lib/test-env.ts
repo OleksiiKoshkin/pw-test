49697b1167333901863e433593ab5ec675b7843c
Oleksii Koshkin <156660851+OleksiiKoshkin@users.noreply.github.com>
2024-10-24 15:31:36 +0200

    rename vars

--- lib/test-env.ts
+++ lib/test-env.ts
@@ -3,9 +3,6 @@ type Target = {
   baseUrl: string
 }
 
-console.log('env', process.env);
-console.log('tenant', process.env.TENANT);
-
 export const testTarget: Target = {
   tenant: process.env.TENANT || '',
   baseUrl: process.env.DOMAIN || ''
